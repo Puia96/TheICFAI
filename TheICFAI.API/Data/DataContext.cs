871165930a39fd5dd3957a1274498f2531536d3b
Ropuia1234 <ctvanlalropuia@gmail.com>
2020-04-28 12:00:00 -0700

    End of Section 8

--- TheICFAI.API/Data/DataContext.cs
+++ TheICFAI.API/Data/DataContext.cs
@@ -9,5 +9,6 @@ namespace TheICFAI.API.Data
         
         public DbSet<Value> Values{ get; set; }
         public DbSet<User> Users { get; set; }
+        public DbSet<Photo> Photos { get; set; }
     }
 }
---
+++ TheICFAI.API/Data/TheICFAIRepository.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TheICFAI.API.Models;
+
+namespace TheICFAI.API.Data
+{
+    public class TheICFAIRepository : ITheICFAIRepository
+    {
+        private readonly DataContext _context;
+        public TheICFAIRepository(DataContext context)
+        {
+            _context = context;
+        }
+        public void Add<T>(T entity) where T : class
+        {
+            _context.Add(entity);
+        }
+
+        public void Delete<T>(T entity) where T : class
+        {
+            _context.Remove(entity);
+        }
+
+        public async Task<User> GetUser(int id)
+        {
+            var user = await _context.Users.Include(p => p.Photos).FirstOrDefaultAsync(u => u.Id == id);
+
+            return user;
+        }
+
+        public async Task<IEnumerable<User>> GetUsers()
+        {
+            var users = await _context.Users.Include(p => p.Photos).ToListAsync();
+
+            return users;
+        }
+
+        public async Task<bool> SaveAll()
+        {
+            return await _context.SaveChangesAsync() > 0;
+        }
+    }
+}
