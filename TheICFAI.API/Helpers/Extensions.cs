51041fce499500f588760c92c5ca09859677ae26
Ropuia1234 <ctvanlalropuia@gmail.com>
2020-05-03 10:38:01 -0700

    End of Section 14

--- TheICFAI.API/Helpers/Extensions.cs
+++ TheICFAI.API/Helpers/Extensions.cs
@@ -1,16 +1,28 @@
 using System;
 using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace TheICFAI.API.Helpers
 {
-     public static class Extensions
+   public static class Extensions
     {
         public static void AddApplicationError(this HttpResponse response, string message)
         {
             response.Headers.Add("Application-Error", message);
             response.Headers.Add("Access-Control-Expose-Headers", "Application-Error");
-            response.Headers.Add("Access-Control-Allow-Origin", "*");   
+            response.Headers.Add("Access-Control-Allow-Origin", "*");
         }
+
+        public static void AddPagination(this HttpResponse response, 
+            int currentPage, int itemsPerPage, int totalItems, int totalPages)
+        {
+            var paginationHeader = new PaginationHeader(currentPage, itemsPerPage, totalItems, totalPages);
+            var camelCaseFormatter = new JsonSerializerSettings();
+            camelCaseFormatter.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            response.Headers.Add("Pagination", JsonConvert.SerializeObject(paginationHeader, camelCaseFormatter));
+            response.Headers.Add("Access-Control-Expose-Headers", "Pagination");
+        }    
 
         public static int CalculateAge(this DateTime theDateTime)
         {
@@ -19,6 +31,7 @@ namespace TheICFAI.API.Helpers
                 age--;
 
             return age;
+            
         }
     }
 }
