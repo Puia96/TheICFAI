41d4f259d14210e03bb455bb1e6f94183251f177
Ropuia1234 <ctvanlalropuia@gmail.com>
2020-05-04 08:34:28 -0700

    End of Section 16

--- TheICFAI-SPA/src/app/app.module.ts
+++ TheICFAI-SPA/src/app/app.module.ts
@@ -34,7 +34,8 @@ import { MemberEditResolver } from './_resolvers/member-edit.resolver';
 import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
 import { ListsResolver } from './_resolvers/lists.resolver';
-
+import { MessagesResolver } from './_resolvers/messages.resolver';
+import { MemberMessagesComponent } from './members/member-messages/member-messages.component';
 
 
 
@@ -61,7 +62,8 @@ export class CustomerHammerConfig extends HammerGestureConfig {
       MemberCardComponent,
       MemberDetailComponent,
       MemberEditComponent,
-      PhotoEditorComponent
+      PhotoEditorComponent,
+      MemberMessagesComponent
    ],
    imports: [
       BrowserModule,
@@ -94,7 +96,8 @@ export class CustomerHammerConfig extends HammerGestureConfig {
       MemberEditResolver,
       { provide: HAMMER_GESTURE_CONFIG, useClass: CustomerHammerConfig},
       PreventUnsavedChanges,
-      ListsResolver
+      ListsResolver,
+      MessagesResolver
    ],
    bootstrap: [
       AppComponent
