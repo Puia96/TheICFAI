26f070e865918e1cb2f3c25414dfe49696982828
Ropuia1234 <ctvanlalropuia@gmail.com>
2020-06-07 11:30:24 -0700

    End of Section 16 F

--- TheICFAI.API/Dtos/UserForRegisterDto.cs
+++ TheICFAI.API/Dtos/UserForRegisterDto.cs
@@ -8,7 +8,7 @@ namespace TheICFAI.API.Dtos
         [Required]
         public string Username { get; set; }
         [Required]
-        // [StringLength(8, MinimumLength = 4, ErrorMessage = "You must satisfy password between 4 and 8")]
+        [StringLength(8, MinimumLength = 4, ErrorMessage = "You must satisfy password between 4 and 8")]
         public string Password { get; set; }
          [Required]
          public string Gender { get; set; }
